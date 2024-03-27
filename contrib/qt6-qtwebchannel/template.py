ace1fecfe7f79055ef2303dfd509d2824e8ce5bd
ttyyls <contact@behri.org>
2024-03-27 17:04:32 +0000

    contrib/qt6-qtwebchannel: update to 6.7.0

--- contrib/qt6-qtwebchannel/template.py
+++ contrib/qt6-qtwebchannel/template.py
@@ -1,5 +1,5 @@
 pkgname = "qt6-qtwebchannel"
-pkgver = "6.6.2"
+pkgver = "6.7.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -20,7 +20,7 @@ license = (
 )
 url = "https://www.qt.io"
 source = f"https://download.qt.io/official_releases/qt/{pkgver[:-2]}/{pkgver}/submodules/qtwebchannel-everywhere-src-{pkgver}.tar.xz"
-sha256 = "3d7c7d3999a394aa337bf575e33e526b058922e3760b34c942e5c8c174dcccc6"
+sha256 = "26bfbd404e0c82bfbeeaef81796eb7a7cf931c5c1a5e942a315ca7126322efff"
 debug_level = 1  # defatten, especially with LTO
 # FIXME
 hardening = ["!int"]
