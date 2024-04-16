1a042b0182c62262c8eb3c347043c81b9b9daf0b
ttyyls <contact@behri.org>
2024-04-16 13:04:16 +0100

    contrib/sww: update to 0.9.3

--- contrib/swww/template.py
+++ contrib/swww/template.py
@@ -1,5 +1,5 @@
 pkgname = "swww"
-pkgver = "0.9.1"
+pkgver = "0.9.3"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo", "pkgconf", "scdoc"]
@@ -9,7 +9,7 @@ maintainer = "Nova <froggo8311@proton.me>"
 license = "GPL-3.0-only"
 url = "https://github.com/LGFae/swww"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "c1f6cf2ccd4878e9cb5b6c6412494120535227516d3749694ac4e75ec68e0547"
+sha256 = "b190f9fa63ca9952e12c8b3982862a75f714af5546e1c5e34adc17f9764b1bf4"
 
 
 def post_build(self):
