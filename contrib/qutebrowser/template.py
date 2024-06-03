42138a1f300de40fd62c3a33cd065b0aba1cb87e
ttyyls <contact@behri.org>
2024-06-03 19:45:25 +0100

    contrib/qutebrowser: update to 3.2.0

--- contrib/qutebrowser/template.py
+++ contrib/qutebrowser/template.py
@@ -1,22 +1,14 @@
 pkgname = "qutebrowser"
-pkgver = "3.1.0"
+pkgver = "3.2.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "asciidoc",
     "gmake",
-    "python",
     "python-build",
     "python-installer",
     "python-setuptools",
     "python-wheel",
-]
-checkdepends = [
-    "python-hypothesis",
-    "python-pytest",
-    "python-pytest-benchmark",
-    "python-pytest-mock",
-    "python-pytest-rerunfailures",
 ]
 depends = [
     "python-adblock",
@@ -32,14 +24,42 @@ depends = [
     "qt6-qtbase-sql",
     "qt6-qtwebengine",
 ]
+checkdepends = depends + [
+    "python-beautifulsoup4",
+    "python-cheroot",
+    "python-flask",
+    "python-hypothesis",
+    "python-jaraco.classes",
+    "python-jaraco.functools",
+    "python-more-itertools",
+    "python-pytest",
+    "python-pytest-bdd",
+    "python-pytest-benchmark",
+    "python-pytest-instafail",
+    "python-pytest-mock",
+    "python-pytest-qt",
+    "python-pytest-rerunfailures",
+    "python-soupsieve",
+    "python-vulture",
+    "xwayland-run",
+]
 pkgdesc = "Keyboard driven web browser with a minimalist gui"
 maintainer = "ttyyls <contact@behri.org>"
 license = "GPL-3.0-only"
 url = "https://qutebrowser.org"
 source = f"https://github.com/qutebrowser/qutebrowser/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "9307d55f1d9157b7906ac08566e3951bffd6f5b753432deaa9a13681995ba3ca"
-# unpackaged dependencies
-options = ["!check"]
+sha256 = "6558bc55bdd7d7a5cefbb9166eea14cd7eaa53ba3e97f6814eb3ebaa548f68e2"
+
+
+def do_check(self):
+    self.do(
+        "xwfb-run",
+        "--",
+        "python",
+        "-m",
+        "pytest",
+        "-q",
+    )
 
 
 def post_install(self):
