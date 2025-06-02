f516e8e9c62ebc7515ec5934ab493ca59025a100
ttyyls <contact@behri.org>
2025-06-03 00:35:07 +0100

    user/cups-pdf: new package

---
+++ user/cups-pdf/template.py
@@ -0,0 +1,13 @@
+pkgname = "cups-pdf"
+pkgver = "3.0.1"
+pkgrel = 0
+makedepends = ["cups-devel"]
+pkgdesc = "CUPS PDF printer"
+license = "GPL-2.0-or-later"
+url = "https://www.cups-pdf.de"
+source = f"https://www.cups-pdf.de/src/cups-pdf_{pkgver}.tar.gz"
+sha256 = "738669edff7f1469fe5e411202d87f93ba25b45f332a623fb607d49c59aa9531"
+
+
+def install(self):
+    pass
