d596b84f48b4a50c6e565c054d0a955bc1fcb9b4
ttyyls <contact@behri.org>
2025-06-02 23:52:32 +0100

    main/thinkfan: update to 2.0.0

--- main/thinkfan/template.py
+++ main/thinkfan/template.py
@@ -1,6 +1,6 @@
 pkgname = "thinkfan"
-pkgver = "1.3.1"
-pkgrel = 1
+pkgver = "2.0.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DCMAKE_POLICY_VERSION_MINIMUM=3.5"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -9,7 +9,7 @@ pkgdesc = "Simple fan control program"
 license = "GPL-3.0-or-later"
 url = "https://github.com/vmatare/thinkfan"
 source = f"https://github.com/vmatare/thinkfan/archive/{pkgver}.tar.gz"
-sha256 = "9466c8c82b7c4333b280fa66445ab26185ffbb4aada6bcb4a164eed742f8d78c"
+sha256 = "0fc94eb378dcba8c889e91f41dab3a8d6eebc7324a59a0704cc39aa66551987e"
 hardening = ["vis"]
 
 
