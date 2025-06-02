a2df993ff616e38c5f3bf2b9a587d93526f61e0c
ttyyls <contact@behri.org>
2025-06-03 00:18:04 +0100

    user/irssi: new package

---
+++ user/irssi/template.py
@@ -0,0 +1,8 @@
+pkgname = "irssi"
+pkgver = ""
+pkgrel = 0
+pkgdesc = "Modular text mode chat client"
+license = "GPL-3.0-or-later"
+url = "https://irssi.org"
+source = f"https://codeberg.org/irssi/irssi/archive/{pkgver}.tar.gz"
+sha256 = ""
