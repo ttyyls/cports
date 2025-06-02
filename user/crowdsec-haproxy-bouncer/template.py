2eb516197bedc2153815f2459c76195e908016ba
ttyyls <contact@behri.org>
2025-06-03 00:21:41 +0100

    user/crowdsec-haproxy-bouncer: new package

---
+++ user/crowdsec-haproxy-bouncer/template.py
@@ -0,0 +1,12 @@
+pkgname = "crowdsec-haproxy-bouncer"
+pkgver = "0.0.6"
+pkgrel = 0
+pkgdesc = "Crowdsec bouncer for HAProxy"
+license = "MIT"
+url = "https://crowdsec.net"
+source = f"https://github.com/crowdsecurity/cs-haproxy-bouncer/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "54d517f92a4f13ed3b1adc3c340cc72f273968455ee5a3dd1d6bfbc8db2a25af"
+
+
+def install(self):
+    pass
