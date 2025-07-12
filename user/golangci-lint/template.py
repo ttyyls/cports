5ce8f9908dd6cd4c26e678f25f6529d242b7d3f7
ttyyls <contact@behri.org>
2025-07-12 20:49:06 +0100

    user/golangci-lint: generate completions

--- user/golangci-lint/template.py
+++ user/golangci-lint/template.py
@@ -1,6 +1,6 @@
 pkgname = "golangci-lint"
 pkgver = "2.1.6"
-pkgrel = 1
+pkgrel = 2
 build_style = "go"
 make_dir = "build-cccc"
 make_build_args = ["./cmd/golangci-lint"]
@@ -10,5 +10,22 @@ license = "GPL-3.0-or-later"
 url = "https://golangci-lint.run"
 source = f"https://github.com/golangci/golangci-lint/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "0ae50c50dbc7603ed27a6f559dca194cb8851f030410c5635270866e78cb3400"
+# cross: generates completions with host binary
 # some tests fail because of chroot and some need network
-options = ["!check"]
+options = ["!check", "!cross"]
+
+
+def post_build(self):
+    for shell in ["bash", "fish", "zsh"]:
+        with open(self.cwd / f"golangci.{shell}", "w") as outf:
+            self.do(
+                "build-cccc/golangci-lint",
+                "completion",
+                shell,
+                stdout=outf,
+            )
+
+
+def post_install(self):
+    for shell in ["bash", "fish", "zsh"]:
+        self.install_completion(f"golangci.{shell}", shell)
