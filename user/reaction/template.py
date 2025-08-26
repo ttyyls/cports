8685016275228edde43f3e38ca5a1432857ea412
ttyyls <contact@behri.org>
2025-08-26 09:24:53 +0100

    user/reaction: update to 2.2.0

--- user/reaction/template.py
+++ user/reaction/template.py
@@ -1,5 +1,5 @@
 pkgname = "reaction"
-pkgver = "2.1.2"
+pkgver = "2.2.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -8,27 +8,16 @@ pkgdesc = "Scans logs for repeated patterns and takes action"
 license = "AGPL-3.0-or-later"
 url = "https://reaction.ppom.me"
 source = f"https://framagit.org/ppom/reaction/-/archive/v{pkgver}/reaction-v{pkgver}.tar.gz"
-sha256 = "894cededfa22d8e1058830828ccedba21bd9ce2f744bbd539b224f9e71f95eea"
-hardening = ["vis", "cfi"]
+sha256 = "b5571cff639e631eb11eb31c73fde15547af774cf2e69311e1c9501edd136e0e"
 
 if self.profile().wordsize == 32:
     broken = "needs atomicu64"
-
-
-def post_build(self):
-    from cbuild.util import compiler
-
-    cc = compiler.C(self)
-    cc.invoke(["helpers_c/ip46tables.c"], "ip46tables")
-    cc.invoke(["helpers_c/nft46.c"], "nft46")
 
 
 def install(self):
     with self.pushd(f"target/{self.profile().triplet}/release"):
         self.install_bin("reaction")
         self.install_man("reaction*.1", glob=True)
-    self.install_bin("ip46tables")
-    self.install_bin("nft46")
     self.install_license("LICENSE")
     self.install_tmpfiles(self.files_path / "tmpfiles.conf")
     self.install_service(self.files_path / "reaction")
